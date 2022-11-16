78ebdabad7e5a8512fabf71f7ee1988469c67d60
Paul Applegate <paul@theapplegates.com>
2022-11-16 15:29:31 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), mdx()],
+  site: `http://docs.paulapplegate.com`
+});
